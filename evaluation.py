7d5e31ba8a1d283b3e12d7c572757037ca1f2c31
Huy Hoang Nguyen <dranh.club@gmail.com>
2022-09-13 14:04:05 +0700

    Train on aiteam server and download model checkpoint

--- evaluation.py
+++ evaluation.py
@@ -10,6 +10,7 @@ from evaluation.evaluate_cc import evaluate_cc
 import sys
 import logging
 from datetime import datetime
+# import matplotlib.pyplot as plt
 
 ################################# Setup logger #################################
 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
@@ -32,7 +33,7 @@ mytransform = transforms.Compose([
 ])
 
 net = deepWBnet()
-net.load_state_dict(torch.load("checkpoints/model_20220908_102925.pth"))
+net.load_state_dict(torch.load("checkpoints/model_20220912_123225_e10.pth"))
 net.eval()
 
 def kernelP(I):
@@ -81,23 +82,31 @@ def infer(image):
     mapping_func = get_mapping_func(image1, output)
     return outOfGamutClipping(apply_mapping_func(image, mapping_func))
 
-def eval(dataset_name = 'RenderedWB_Set1', imgin = '8D5U5529_D_CS.jpg'):
-    logger.info(f'{dataset_name}/{imgin}')
-
-    in_base = r'E:\datasets\WB\Set1\Set1_input_images_wo_cc_JPG'
-    gt_base = r'E:\datasets\WB\Set1\Set1_ground_truth_images_wo_cc'
+def eval(name, imgin, gtimg):
+    logger.info(f'RenderedWB_Set1/{name}')
     metadata_base = r'E:\datasets\WB\Set1\Set1_input_images_metadata'
 
     # read the image
-    I_in = cv2.imread(os.path.join(in_base, imgin), cv2.IMREAD_COLOR)
+    I_in = cv2.imread(imgin, cv2.IMREAD_COLOR)
     # read gt image
-    metadata = get_metadata(imgin, dataset_name, metadata_base)  
-    gt = cv2.imread(os.path.join(gt_base, metadata["gt_filename"]), cv2.IMREAD_COLOR)
-
+    gt = cv2.imread(gtimg, cv2.IMREAD_COLOR)
+    # metadata
+    metadata = get_metadata(name, 'RenderedWB_Set1', metadata_base)  
 
     # white balance I_in
     I_corr = (infer(I_in[:,:,::-1] / 255)[:,:,::-1]*255).astype("uint8")  
 
+    # plt.figure(figsize=(20,10))
+    # plt.subplot(1,3,1)
+    # plt.imshow(I_in[:,:,::-1])
+    # plt.title("Input")
+    # plt.subplot(1,3,2)
+    # plt.imshow(gt[:,:,::-1])
+    # plt.title("Target")
+    # plt.subplot(1,3,3)
+    # plt.imshow(I_corr[:,:,::-1])
+    # plt.title("Corrected")
+    # plt.show()
 
     # Evaluation
     deltaE00, MSE, MAE, deltaE76 = evaluate_cc(I_corr, gt, metadata["cc_mask_area"],
@@ -108,8 +117,14 @@ def eval(dataset_name = 'RenderedWB_Set1', imgin = '8D5U5529_D_CS.jpg'):
     return deltaE00, MSE, MAE, deltaE76
 
 if __name__ == "__main__":
-    img_names = os.listdir(r'E:\datasets\WB\Set1\Set1_input_images_wo_cc_JPG')
-    
+    img_dir = r'E:\datasets\WB\Set1\Set1_input_images_wo_CC_JPG'
+    gt_dir = r'E:\datasets\WB\Set1\Set1_ground_truth_images_wo_CC'
+    fold_file = r"E:\datasets\WB\Set1\Set1_folds\fold_1.txt"
+
+    imgs = []    
+    with open(fold_file, 'r') as f:
+        imgs += [s.strip().split(".")[0] for s in f.readlines()]
+
     sum_deltaE00 = 0
     sum_MSE = 0 
     sum_MAE = 0
@@ -118,7 +133,9 @@ if __name__ == "__main__":
     n = 10
     for i in range(n):
         logger.info(f'{i+1}/{n}')
-        deltaE00, MSE, MAE, deltaE76 = eval(imgin=img_names[i])
+        img_in = os.path.join(img_dir, imgs[i] + '.jpg')
+        img_gt = os.path.join(gt_dir, imgs[i].rsplit('_', maxsplit=2)[0] + '_G_AS.png')
+        deltaE00, MSE, MAE, deltaE76 = eval(imgs[i], img_in, img_gt)
         sum_deltaE00 += deltaE00
         sum_MSE += MSE
         sum_MAE += MAE
